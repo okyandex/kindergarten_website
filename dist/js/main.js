957e372766b72c8b2df0b72e4243aa009bd1674a
okyandex <avznn@yandex.ru>
2019-11-18 17:46:40 +0300

    glide js slider added

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".header__burger").click(function(e){$(".header__burger,.main-menu").toggleClass("active"),$("body").toggleClass("lock")})});
+$(document).ready(function(){$(".header__burger").click(function(e){$(".header__burger,.main-menu").toggleClass("active"),$("body").toggleClass("lock")}),new Glide(".glide",{type:"carousel",startAt:1,perView:2,breakpoints:{768:{perView:1}}}).mount()});
--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,15 @@ $(document).ready(function() {
         $('.header__burger,.main-menu').toggleClass('active');
         $('body').toggleClass('lock');
 
-    })
+    });
+    new Glide('.glide', {
+        type: 'carousel',
+        startAt: 1,
+        perView: 2,
+        breakpoints: {
+            768: {
+                perView: 1
+            }
+        }
+    }).mount()
 })
